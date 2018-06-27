2dd6206119cf4f8329299452d4adb9e55fc33c63
AntonCoon <eliseevantoncoon@gmail.com>
2018-06-28 02:06:35 +0300

    plot

--- corr_pca_rndf/importance_plot.R
+++ corr_pca_rndf/importance_plot.R
@@ -1,8 +1,22 @@
-imp.data <- data.frame(nms = (attributes(res.forest$importance)$dimnames)[[1]], val = c(res.forest$importance))
+sorted.importance.dataframe <- function(res.forest){
+    imp.data <- data.frame(
+        nms = (attributes(res.forest$importance)$dimnames)[[1]], 
+        val = c(res.forest$importance))
+    imp.data$nms <- factor(imp.data$nms, levels = imp.data$nms[order(imp.data$val)])
+    return(imp.data)
+}
 
-imp.data <- imp.data[order(imp.data$val, decreasing = T), ]
-plot(imp.data$val)
-ggplot(imp.data, aes(x = nms, y = val)) +
-    geom_bar(stat="identity", fill="steelblue") + 
-    theme_minimal() +
-    theme(axis.text.x = element_text(angle = 90, hjust = 1))
+important.bar.plot <- function(res.forest, ...){
+    ggplot(sorted.importance.dataframe(res.forest), aes(x = nms, y = val)) +
+        geom_bar(stat="identity", fill = "steelblue", alpha = 0.7) +
+        theme_minimal() +
+        theme(axis.text.x = element_text(angle = 75, hjust = 1), ...)
+}
+
+important.dot.plot <- function(res.forest, ...){
+    ggplot(sorted.importance.dataframe(res.forest), aes(x = nms, y = val)) + 
+        geom_point(col = "steelblue") + 
+        theme_minimal() +
+        theme(axis.text.x = element_text(angle = 75, hjust = 1), ...) +
+        geom_line(aes(x = nms, y = val, group=1), alpha = 0.4)
+}
